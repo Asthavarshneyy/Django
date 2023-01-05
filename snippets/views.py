f58164f92f53603fdb86129a32731dd47f607c20
Astha Varshney <astha119btcse21@igdtuw.ac.in>
2023-01-05 14:07:25 +0530

    Associating snippets with users

--- snippets/views.py
+++ snippets/views.py
@@ -10,9 +10,22 @@
 from rest_framework import generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
 
 
 # Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
